017b817973712748f86906738bf2fb5f792db553
Ka1s3r <cornelduhaney@gmail.com>
2022-06-23 13:00:00 -0400

    Add files via upload
    Adding key images, changing app layout

--- coldpress_app.py
+++ coldpress_app.py
@@ -114,19 +114,7 @@ with st.container():
 #     lighthouse_image = 'lighthouse_waves.JPEG'
 #     st.image(lighthouse_image, width=200)
 
-st.title('ColdPress Lighthouse')
-st.write('---')
-st.write("""
-   To create this app, we had to curate data from prominent and popular media publications.
-   We scraped available APIs and created dataframes based on the prominence of topics such as murder and crime.
-   We then used this data to create a web application with visuals that represent different demographic information in regards to topics that show up frequently in the news. 
-   There are interactive widgets that allow a reader to be able to get different interpretations of the data, along with time periods to narrow down the search. 
-   This allows the data presented to be digestible, and specific to a reader’s needs.      
-
-
-
-        """)
-st.write("---")
+st.title('Career Team 2: ColdPress')
 
 # This line of code will present the heat graph and the statement explaining the map
 with st.container():
@@ -134,18 +122,16 @@ with st.container():
     col1, col2 = st.columns(2)
     with col1:
         st.title("Hypothesis: ")
-        st.write("""
-        Hypothesis:
+        st.write(""" On the question of correlation between events occuring in the real world, and the rate at which articles about those events are published:
 
-There is not a strong correlation between the rate at which events occur in the real world, and the rate at which articles about those events are published.
+Ho: The correlation between events and their report-rate is strong (r >= 0.75). 
 
-Ho: The correlation between events and their report-rate is strong.
-
-Ha: The correlation between events and their report-rate is not strong.
+Ha: The correlation between events and their report-rate is not strong (r < 0.75).
         
         """)
     with col2: 
-        'insert an image '
+        question_image = 'question_mark_PNG56.png'
+        st.image(question_image, width=200)
 
 
 with st.container():
@@ -155,7 +141,13 @@ with st.container():
         st.title("Methods:")
         st.write("""
 
-1. Collected data from (INSERT SOURCES HERE)
+1. These data were taken from the New York Times API, and can be found here: 
+
+https://www.kaggle.com/datasets/tumanovalexander/nyt-articles-data
+   
+   Data on the murder rate for each year were taken from the FBI database and can be found here: 
+   
+   https://ucr.fbi.gov/crime-in-the-u.s/2019/crime-in-the-u.s.-2019/topic-pages/tables/table-1
 
 2. These data were retrieved in the form of 60 csvs, one for each year from 1960 to 2019. These csv’s were then turned into a glob-object using the glob library. We iterated through the object, turning each csv into a Pandas DataFrame, concatenating them vertically to create a time series based dataframe in Pandas. 
 
@@ -163,18 +155,24 @@ with st.container():
 
 4. We created a function which takes a DataFrame where years are the time measure, and steps through a range of years, counting the number of times we encounter an event (articles in this case) for each year.
 
-5. This was used to engineer a new Series called “article_count” which gives us the number of articles for each year in our sub_DataFrame related to murders.
+5. This was used to engineer a new Series called “article_count” which gives us the number of articles for each year in our sub_DataFrame related to murders.      
+        """)
+    with col2: 
+        nyt_image = '05insider-gun-sub-a-superJumbo.jpg'
+        st.image(nyt_image, width=300)
 
-6. We also engineered a similar column by applying the same function to the main DataFrame containing all articles. This is done so that we can find out what percentage of articles in a given year were about a particular topic with regard to that particular news agency. In our demo case, the New York Times on the topic of “murder”.
+        fbi_image = '920x920.jpg'
+        st.image(fbi_image, width=300)
+
+        st.write(""" 6. We also engineered a similar column by applying the same function to the main DataFrame containing all articles. This is done so that we can find out what percentage of articles in a given year were about a particular topic with regard to that particular news agency. In our demo case, the New York Times on the topic of “murder”.
 
 7. To test this, we used FBI data from the same year range as the dataset after turning it into a Series.
 
 8. In all, we have 5 Series: “Years”, “murder_article_count”, ”murder_rate”, “murder_article_rate_,  “all_article_count”.
 
-9. We concatenated these series horizontally to create a single DataFrame.
-           
-        
-        """)
+9. We concatenated these series horizontally to create a single DataFrame. """)
+
+
 with st.container():
     st.write('---')
     st.title("Results:")
@@ -182,6 +180,7 @@ with st.container():
     
     
     """)
+
     col1, col2 = st.columns(2)
     with col1:
         st.header("Correlation Heat Map")
@@ -189,14 +188,18 @@ with st.container():
         sns.heatmap(m_df.corr(), ax=ax, center=0, annot=True, cmap='coolwarm')
         st.write(corr_murders)
         st.write(""" 
-        With 8,857,300 rows of data, we have enough statistical power to show that on the topic of murder, the rate at which the New York Times publishes articles about said topic, has a moderate correlation with the rate at which events occur over time, with a correlation coefficient (Pearson’s) of 0.63.
+        With 8,857,300 rows of data total, and 53,715 for our sub-DataFrame, we have enough statistical power to show that on the topic of murder, the rate at which the New York Times publishes articles about said topic, has a moderate correlation with the rate at which events occur over time, with a correlation coefficient (Pearson’s) of 0.63.
         """)
     with col2:
-        st.write("Heatmap explained: ")
-        st.write('''
- As murder goes up, so do the number of articles about murder.
- As the years go by, the murder rate decreases.
- As the years go by, however, the article count is only weakly tied to that.
+        st.write(''' ---
+ As the murder rate goes up, so too does the rate at which articles about murder are published.
+ This is however, a moderate effect, and does not support the hypothesis that there is a strong correlation between real world event-rates and their publication-rates.
+ 
+ 
+ The rate of publication was chosen instead of the raw number of articles each year, since article count varies with the resources available to the news agency. 
+ 
+ (Staff fluctuations: https://www.statista.com/statistics/192894/number-of-employees-at-the-new-york-times-company/), and other hidden factors.
+ For reference, the murder rate had a weak correlation with the number of articles published about murder in any given year; r = 0.39. 
         ''')
 with st.container():
     col1, col2, col3 = st.columns(3)
@@ -219,7 +222,7 @@ with st.container():
         st.write(fig)
     # values for the select box will return as a tuple.
 
-    sel_box_opt = st.slider("Select the year ",value = (1959,2019), min_value = 1959, max_value = 2019, step=10)
+    sel_box_opt = st.slider("SELECT A YEAR RANGE",value = (1959,2019), min_value = 1959, max_value = 2019, step=10)
     plot_filter_data(year_range(sel_box_opt[0], sel_box_opt[1]))
 #[I'm having an  error in line 248, it is not accepting my function]
            
@@ -243,10 +246,8 @@ with st.container():
         plt.show()
         st.write(fig3)
 
-st.header("Result: ")
-st.write("""The results we got from comparing our publication data to the real word values have led us to believe that news publications typically go for shock value over substance.
- When compared to data that comes from real life situations, we have observed that a lot of publications go for the more shocking and horrifying occurrences, the frequency of which may not an accurate foil to real life situations.
- """)
+
+st.write(""" A closer look at the comparison, where the ranges are more comparable shows the afformentioned moderate correlation.""")
 
 # registering to our app sidebar
 # At the moment registering to this app is not needed
@@ -349,7 +350,7 @@ st.write("""The results we got from comparing our publication data to the real w
 #creating a multiselect search for key works to show count of relating article publish 
 #The future venture of the app
 
-st.header("Discussion")
+st.header("Discussion:")
 st.write("""ColdPress was born out of the knowledge that news has a tendency to report on things in a biased fashion. 
 This divide is pretty visible in our everyday life, and in the conversations we have with others. 
 With the data that we have curated from various publications and APIs, we wanted to create an app that shows you how frequently various news topics are mentioned as compared to how often these things actually occur. 
@@ -359,10 +360,7 @@ The future of ColdPress would provide a hub for unbiased and clear data on news 
 ColdPress would include more data on different statistics that frequently show up in the news, such as immigration, assault, and other hot button issues without being particularly triggering to the reader and without eliciting fluff pieces or op eds that could lead a reader to a particular view point. 
 ColdPress should allow for readers to see the grand scale of the issues they’re interested in researching. Overall, the mission statement is plain and simple, to keep the user informed about the degree to which a particular news agency deviates from reality in terms of how information is passed on to the reader. 
 ColdPress will hopefully help in understanding our news, our world, and each other a little better. 
-
 """)
-
-
 
 with st.container():
     path_2 = 'sample_df_7300.csv'
